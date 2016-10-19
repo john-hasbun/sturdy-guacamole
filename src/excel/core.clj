0e553696c51608fe25148c76ac98e580fb25b856
Jonathan Hasbun <jonathan.hasbun@webtrends.com>
2016-10-19 14:02:13 -0700

    Added test for redirect

--- src/excel/core.clj
+++ src/excel/core.clj
@@ -8,11 +8,19 @@
 (defn redirect-counter []
   (def counter (inc counter)))
 
+;; Test refresh
 (defn request-handler [request]
   (redirect-counter)
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (str "<html><table><tr><th>" counter "</th></tr></table></html>")})
+
+;; Test redirect
+;; (defn request-handler [request]
+;;   (redirect-counter)
+;;   {:status 302
+;;    :headers {"Location" (str "http://localhost:4895?redirect=" counter)}
+;;    :body (str "<html><table><tr><th>" counter "</th></tr></table></html>")})
 
 
 (defn -main [& args]
